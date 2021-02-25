73da37dc102ae29cad70ac7063215804ee5bb25d
commandoby <beresten20117@mail.ru>
2021-02-25 21:12:25 +0300

    First commit

---
+++ src/HW_05_02_2021/Task1_ConsoleEnter.java
@@ -0,0 +1,10 @@
+package HW_05_02_2021;
+//Имеется прямоугольное отверстие размерами a и b.
+//Определить можно ли его закрыть кругом с радиусом r.
+//Вывести результат на экран.
+
+public class Task1_ConsoleEnter {
+    public static void main(String[] args) {
+
+    }
+}
