b9cf344898a49776169902600a3431d043ff7d72
commandoby <beresten20117@mail.ru>
2021-03-11 18:43:13 +0300

    Commit hw_210312_1

---
+++ src/HW_210312/Task2_Cycles.java
@@ -0,0 +1,15 @@
+package HW_12_03_2021;
+//Одноклеточная амеба каждые 3 часа делится на 2 клетки.
+//Необходимо определить, сколько амеб будет через 3, 6, 9, 12,..., 24 часа.
+
+public class Task2_Cycles {
+    public static void main(String[] args) {
+        int ameba = 1;
+        int hour = 0;
+        for (int i = 0; i < 8; i++) {
+            ameba *= 2;
+            hour += 3;
+            System.out.printf("Через %d часа(ов) будет %d амёб. \n", hour, ameba);
+        }
+    }
+}
